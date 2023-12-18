13a8ad63325b263052fe81ebf3e2a72c0b594456
Sander <sander.borret@hotmail.com>
2023-12-18 17:16:20 +0100

    Include waitForKeyElements in code instead of a require
    Greasyfork ugh

--- youtube-more-speeds.user.js
+++ youtube-more-speeds.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      1.5
+// @version      1.5.1
 // @name         YouTube More Speeds
 // @description  Adds buttons under a YouTube video with more playback speeds.
 
@@ -312,7 +312,6 @@
 // @homepage https://github.com/ssssssander/youtube-more-speeds
 // @supportURL https://github.com/ssssssander/youtube-more-speeds/issues
 // @match        *://*.youtube.com/*
-// @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
 // @license MIT
 // ==/UserScript==
 
@@ -322,13 +321,75 @@
 (function() {
     'use strict';
 
+    // BEGIN waitForKeyElements
+    /**
+ * A utility function for userscripts that detects and handles AJAXed content.
+ *
+ * Usage example:
+ *
+ *     function callback(domElement) {
+ *         domElement.innerHTML = "This text inserted by waitForKeyElements().";
+ *     }
+ *
+ *     waitForKeyElements("div.comments", callback);
+ *     // or
+ *     waitForKeyElements(selectorFunction, callback);
+ *
+ * @param {(string|function)} selectorOrFunction - The selector string or function.
+ * @param {function} callback - The callback function; takes a single DOM element as parameter.
+ *                              If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean} [waitOnce=true] - Whether to stop after the first elements are found.
+ * @param {number} [interval=300] - The time (ms) to wait between iterations.
+ * @param {number} [maxIntervals=-1] - The max number of intervals to run (negative number for unlimited).
+ */
+    function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals) {
+        if (typeof waitOnce === "undefined") {
+            waitOnce = true;
+        }
+        if (typeof interval === "undefined") {
+            interval = 300;
+        }
+        if (typeof maxIntervals === "undefined") {
+            maxIntervals = -1;
+        }
+        var targetNodes = (typeof selectorOrFunction === "function")
+        ? selectorOrFunction()
+        : document.querySelectorAll(selectorOrFunction);
+
+        var targetsFound = targetNodes && targetNodes.length > 0;
+        if (targetsFound) {
+            targetNodes.forEach(function(targetNode) {
+                var attrAlreadyFound = "data-userscript-alreadyFound";
+                var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+                if (!alreadyFound) {
+                    var cancelFound = callback(targetNode);
+                    if (cancelFound) {
+                        targetsFound = false;
+                    }
+                    else {
+                        targetNode.setAttribute(attrAlreadyFound, true);
+                    }
+                }
+            });
+        }
+
+        if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+            maxIntervals -= 1;
+            setTimeout(function() {
+                waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals);
+            }, interval);
+        }
+    }
+    // END waitForKeyElements
+
+    // Actual code starts here
     let funcDone = false;
     const titleElemSelector = 'div#title.style-scope.ytd-watch-metadata';
     const colors = ['#072525', '#287F54', '#C22544']; // https://www.schemecolor.com/wedding-in-india.php
     if (!funcDone) window.addEventListener('yt-navigate-start', addSpeeds);
 
     if (document.body && !funcDone) {
-        waitForKeyElements(titleElemSelector, addSpeeds); // eslint-disable-line no-undef
+        waitForKeyElements(titleElemSelector, addSpeeds);
     }
 
     function addSpeeds() {
